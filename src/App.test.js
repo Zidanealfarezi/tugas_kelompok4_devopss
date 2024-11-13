873a415111d69cb99291299dbec8d9d73d973615
zidanealfarezi <zidanealfarezi89@gmail.com>
2024-11-13 15:48:01 +0700

    memperbaiki ci/cd

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders a button with text SHOW", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByText(/show/i); // Mencari tombol dengan teks SHOW
+  expect(buttonElement).toBeInTheDocument();
 });
